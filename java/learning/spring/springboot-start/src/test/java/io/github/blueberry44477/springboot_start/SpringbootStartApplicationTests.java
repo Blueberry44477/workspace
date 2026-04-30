8b0c5670391f674912a9e252bb6023bd00734119
Blueberry44477 <vas172674@gmail.com>
2026-04-30 14:17:55 +0300

    refactor: restructure directory layout
    move all code one directory back
    remove matcad labs
    rename software engineering principles labs dir

---
+++ java/learning/spring/springboot-start/src/test/java/io/github/blueberry44477/springboot_start/SpringbootStartApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.blueberry44477.springboot_start;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootStartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
